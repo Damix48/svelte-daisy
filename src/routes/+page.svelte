0dbe9153c7ef5b923793c5ab40aa0f359326d22e
Damiano Zanardo <damiano.zanardo@gmail.com>
2026-03-31 10:31:15 +0200

    fix opened on sm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-import { Modal, modalService } from "$lib";
+import { Dropdown, Modal, modalService } from "$lib";
 import Select from "$lib/components/ui/select/Select.svelte";
 import FormRoot from "$lib/Form/FormRoot.svelte";
+import TreeSelect from "$lib/components/ui/select/TreeSelect.svelte";
 import Ciao from "./ciao.svelte";
+import { RouteOff } from "@lucide/svelte";
 
 type User = { id: number; name: string };
 let users: User[] = [
@@ -20,7 +22,74 @@ let user: User | undefined = $state();
 let userId: number | undefined;
 let multiUsers: User[] = $state([]);
 let multiId: number[] = $state([]);
+
+type Node = {
+  id: number;
+  name: string;
+  children?: Node[];
+};
+
+const treeData: Node[] = [
+  {
+    id: 1,
+    name: "Node 1",
+    children: [
+      { id: 2, name: "Child 1" },
+      { id: 3, name: "Child 2" }
+    ]
+  },
+  {
+    id: 4,
+    name: "Node 2",
+    children: [
+      { id: 5, name: "Child 3" },
+      { id: 6, name: "Child 4" },
+      { id: 7, name: "Child 5", children: [{ id: 8, name: "Grandchild 1" }] }
+    ]
+  },
+  {
+    id: 9,
+    name: "Node 3",
+    children: [
+      { id: 10, name: "Child 6" },
+      { id: 11, name: "Child 7" }
+    ]
+  },
+  {
+    id: 121,
+    name: "Node 3",
+    children: [
+      { id: 122, name: "Child 6" },
+      { id: 123, name: "Child 7" }
+    ]
+  },
+  {
+    id: 124,
+    name: "Node 3",
+    children: [
+      { id: 125, name: "Child 6" },
+      { id: 126, name: "Child 7" }
+    ]
+  }
+];
+
+let selectedNodes: Node[] = $state([]);
 </script>
+
+<Dropdown.Root>
+  <Dropdown.Trigger class="btn">Open Dropdown</Dropdown.Trigger>
+  <Dropdown.Content>
+    <ul class="menu p-2">
+      <li><a>Item 1</a></li>
+      <li><a>Item 2</a></li>
+      <li><a>Item 3</a></li>
+    </ul>
+  </Dropdown.Content>
+</Dropdown.Root>
+
+<input class="input" type="text" placeholder="Type here">
+<TreeSelect items={treeData} itemToId={(x) => x.id} itemToString={(x) => x.name} bind:selected={selectedNodes} type="multiple" cascadeSelection selectionMode="leaf" />
+<TreeSelect items={treeData} itemToId={(x) => x.id} itemToString={(x) => x.name} bind:selected={selectedNodes} type="multiple" cascadeSelection selectionMode="any" />
 
 <Modal.Root>
   <Modal.Trigger class="btn"> Open Modal</Modal.Trigger>
